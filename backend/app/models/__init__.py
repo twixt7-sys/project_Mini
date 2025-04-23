1d0d5606f76a8687d6c1b61975c5764223372305
bloo <nexushellstein@gmail.com>
2025-04-23 22:42:11 +0800

    initialized SQL Alchemy models
    - user
    - post
    - like
    - comment
    - followers

--- backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-# models/__init__.py
-from .user import User
-from .post import Post
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,9 +1,26 @@
-from sqlalchemy import Column, Integer, String
-from models import Base  # or from models import Base
+from app.extensions import db
+from datetime import datetime as dt
 
-class User(Base):
-	__tablename__ = "users"
-	
-	id = Column(Integer, primary_key=True)
-	username = Column(String, unique=True, nullable=False)
-	email = Column(String, unique=True, nullable=False)
+class User(db.Model):
+    __tablename__ = 'users'
+
+    #attributes
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    created_at = db.Column(db.DateTime,  default=dt.now(dt.timezone.utc))
+    
+    #relationships
+    posts = db.relationship('Post', backref='user', lazy=True)
+    likes = db.relationship('Like', backref='user', lazy=True)
+    comments = db.relationship('Comment', backref='user', lazy=True)
+    
+    # followers relationship (many-to-many)
+    following = db.relationship(
+		'User',
+		secondary='followers',
+		primaryjoin='User.id==followers.c.follower_id',
+		secondaryjoin='User.id==followers.c.followed_id',
+		backref='followers'
+	)
