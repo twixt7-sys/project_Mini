45fa90d335f00135c046524211352b36b653712e
bloo <nexushellstein@gmail.com>
2025-04-26 07:29:57 +0800

    implemented: successful database connection & table creation
    SQL Alchemy models fully working

--- backend/app/models/followers.py
+++ backend/app/models/followers.py
@@ -1,8 +1,9 @@
 from sqlalchemy import Column, Integer, ForeignKey, Table
-
+from extensions import Base
 
 # imperative mapping
 followers = Table('followers',
+    Base.metadata,
     Column('follower_id', Integer, ForeignKey('users.id'), primary_key=True),
     Column('followed_id', Integer, ForeignKey('users.id'), primary_key=True)
 )
