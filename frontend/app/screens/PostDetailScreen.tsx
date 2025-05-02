b109b9179d0f1ffdbf68326e5344bff52f71501c
bloo <nexushellstein@gmail.com>
2025-05-02 14:51:53 +0800

    Initialized PostDetailScreen

---
+++ frontend/app/screens/PostDetailScreen.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import { RouteProp, useRoute } from '@react-navigation/native'
+import { Post } from '../types/Post'
+
+type PostDetailRoute = RouteProp<{ params: { post: Post } }, 'params'>
+
+const PostDetailScreen = () => {
+	const { params } = useRoute<PostDetailRoute>()
+	const { post } = params
+
+	return (
+		<ScrollView contentContainerStyle={{ padding: 20 }}>
+			<Text style={{ fontSize: 24, fontWeight: 'bold' }}>{post.title}</Text>
+			<Text>{post.content}</Text>
+			<Text>By: {post.author}</Text>
+		</ScrollView>
+	)
+}
+
+export default PostDetailScreen
