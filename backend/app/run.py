06d1bddecf8f49a208aadb0be086b7d0a8b88505
bloo <nexushellstein@gmail.com>
2025-06-01 19:43:22 +0800

    removed some imports

--- backend/app/run.py
+++ backend/app/run.py
@@ -1,11 +1,10 @@
 from flask import request, jsonify
 from extensions import SessionLocal
 from app import create_app
-from models import post as p
-from models import user as u
 from routes.post_routes import post_bp
-from marshmallow import Schema, fields, ValidationError
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from routes.auth_routes import auth_bp
 
 app = create_app()
 CORS(app)
