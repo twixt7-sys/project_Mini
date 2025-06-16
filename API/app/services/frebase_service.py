4782858c52de62b9e0aa152223d9391a6ebb7ed5
bloo <nexushellstein@gmail.com>
2025-06-16 20:01:29 +0800

    modified firebase_service.py

--- API/app/services/frebase_service.py
+++ API/app/services/frebase_service.py
@@ -1,11 +1,14 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# load credentials
-cred = credentials.Certificate("../../firebaes_config.json")
+firebase_app, db = None, None
 
-# initialize firebase
-firebase_admin.initialize_app(cred)
-
-# get db instance
-db = firestore.client()
+def init_firebase():
+    global firebase_app, db
+    if not firebase_admin._apps:
+        # load credentials
+        cred = credentials.Certificate("../../firebaes_config.json")
+        # initialize firebase
+        firebase_admin.initialize_app(cred)
+        # get db instance
+        db = firestore.client()
