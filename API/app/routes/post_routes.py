a57c4f7653c715a91fea8f2af0a016840732daa0
bloo <nexushellstein@gmail.com>
2025-06-18 12:48:17 +0800

    initialized post index route

--- API/app/routes/post_routes.py
+++ API/app/routes/post_routes.py
@@ -4,9 +4,14 @@ from app.utilities.Utils import response, error
 
 post_bp = Blueprint('posts', __name__, url_prefix='/api/posts')
 
+# TODO: develop get_posts route
 @post_bp.route('/', methods=['GET'])
 def get_posts():
     try:
-        posts_ref = db.collection('posts')
+        # get all posts
+        docs = db.collection('posts').stream()
+        # add all dict-converted-posts in list
+        posts = [posts.append(doc.to_dict()) for doc in docs]
+        return response("success", "Retrieved all posts", posts)
     except Exception as e:
         return error("error" , str(e), 500)
